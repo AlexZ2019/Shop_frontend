05c5ffa40c4a00cea9bf83b1f167f019f3fb9d3e
Alex <alex@konstankino.com>
2022-03-26 17:07:17 +0200

    add config for Apollo, routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import {ApolloProvider} from "@apollo/client";
+import {client} from "./Apollo/config";
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <ApolloProvider client={client}>
+          <BrowserRouter>
+              <div className="App">
+              </div>
+          </BrowserRouter>
+      </ApolloProvider>
   );
 }
 
