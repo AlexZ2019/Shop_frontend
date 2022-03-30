07ccc5d6b8bd8fa1b5c6b14d0de7c50956a265e3
Alex <alex@konstankino.com>
2022-03-30 16:45:19 +0300

    clean up

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,4 @@ import routePaths from "./routePaths";
 export default {
     ...routePaths
 }
+
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -4,7 +4,6 @@ import * as whetherForecast from "./whetherForecast"
 import {mergeArrays} from "../utils/reduceArrays";
 
 let routes = mergeArrays(auth.routes, whetherForecast.routes)
-console.log("routes", routes)
 export {auth, common, whetherForecast, routes}
 
 
