1831cc0a60a05a94fbaae51dc8b170e24689eb0c
Alex <alex@konstankino.com>
2022-05-06 09:43:16 +0300

    added index file for user and city models

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -3,7 +3,7 @@ import auth from './auth';
 import whetherForecast from './weather';
 import { IModule } from './common/interfaces/moduleInterfaces';
 
-const routes = mergeArrays(auth.routes, whetherForecast.routes);
+const routes = mergeArrays(auth.routes, whetherForecast.routes );
 
 const module: IModule = { routes };
 export default module;
