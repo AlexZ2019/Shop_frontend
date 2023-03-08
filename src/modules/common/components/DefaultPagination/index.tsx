755c5f37a163cf884e5fbdcfedfdc24a9cb938ba
Alex <alex@konstankino.com>
2023-03-08 10:39:34 +0200

    Added default page size for pagination

--- src/modules/common/components/DefaultPagination/index.tsx
+++ src/modules/common/components/DefaultPagination/index.tsx
@@ -4,12 +4,13 @@ import { OperationVariables, QueryLazyOptions } from "@apollo/client";
 
 const DefaultPagination = ({ total, getNewPage }: { total: number, getNewPage: (options?: (QueryLazyOptions<OperationVariables> | undefined)) => void }) => {
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pageSize] = useState<number>(9);
   const onPageChange = (page: number) => {
     setCurrentPage(page);
     getNewPage({ variables: { page }});
   }
 
-  return <Pagination current={currentPage} total={total} onChange={onPageChange} />
+  return <Pagination current={currentPage} total={total} pageSize={pageSize} onChange={onPageChange} />
 }
 
 export default DefaultPagination;
